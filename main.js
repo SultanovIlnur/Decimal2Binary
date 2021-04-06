3e3cd448b3a07f7bc101f4d1e1bb8237e5d04a03
SultanovIlnur <68385484+SultanovIlnur@users.noreply.github.com>
2021-04-07 00:38:22 +0300

    final fixes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function binaryToDecimal(){
-  let binary = document.getElementById('binaryInput').value;
+  let binary = parseInt(document.getElementById('binaryInput').value, 10);
   if (Number.isInteger(binary)){
     let decimal = parseInt(binary,2);
     document.getElementById('outputText').innerHTML = decimal;
