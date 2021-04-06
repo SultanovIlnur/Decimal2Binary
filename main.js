364f6dcf7f60d061db9332ecbcdf796521b097de
SultanovIlnur <68385484+SultanovIlnur@users.noreply.github.com>
2021-04-06 23:49:20 +0300

    added check for decimal value

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 function binaryToDecimal(){
   let binary = document.getElementById('binaryInput').value;
   let decimal = parseInt(binary,2);
-  document.getElementById('outputText').innerHTML = decimal;
+  if (Number.isInteger(decimal)){
+    document.getElementById('outputText').innerHTML = decimal;
+  }
+  else{
+    document.getElementById('outputText').innerHTML = "Not a decimal!";
+  }
+  
 }
