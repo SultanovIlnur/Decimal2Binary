45a39a576bb49fb42a606b5e792ac7c81edfa9ae
SultanovIlnur <68385484+SultanovIlnur@users.noreply.github.com>
2021-04-14 00:46:29 +0300

    removed inline functions and added copy to clipboard btn

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
+const convertButton = document.getElementById('convertButton');
+const copyToClipboardButton = document.getElementById('copyToClipboardButton');
+const outputText = document.getElementById('outputText');
+
+convertButton.addEventListener('click', binaryToDecimal);
+copyToClipboardButton.addEventListener('click', copyToCB);
+
 function binaryToDecimal(){
   let binary = parseInt(document.getElementById('binaryInput').value, 10);
   if (Number.isInteger(binary)){
     let decimal = parseInt(binary,2);
-    document.getElementById('outputText').innerHTML = decimal;
+    outputText.innerHTML = decimal;
   }
   else{
-    document.getElementById('outputText').innerHTML = "Not a decimal!";
+    outputText.innerHTML = "Not a decimal!";
   }
   
+}
+
+function copyToCB(){
+  let r = document.createRange();
+  r.selectNode(outputText);
+  window.getSelection().addRange(r);
+  document.execCommand('copy');
 }
